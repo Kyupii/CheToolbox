d9e7d9000491c5c6a88ea44363f08d26d8c3b5ba
ethanmolnar06 <157933281+ethanmolnar06@users.noreply.github.com>
2024-02-22 20:06:11 -0500

    set vaplineH and liqlineH correctly as upper and lower eqs

--- chetoolbox/separations.py
+++ chetoolbox/separations.py
@@ -564,14 +564,14 @@ def ponchon_savarit_full_est(eq_curve: common.EqualibEq, liqlineH: common.Linear
   
   tieline, Rmin, R, P = ponchon_savarit_tieline(liqlineH, vaplineH, xf, yf, xd, Rmin_mult).unpack()
 
-  min_stages = common.curve_bouncer(liqlineH, vaplineH, xd, xb, eq_curve.inv)
+  min_stages = common.curve_bouncer(vaplineH, liqlineH, xd, xb, eq_curve.inv)
 
   def y_transform(y):
     line = common.point_conn( (vaplineH.inv(y), y), (xd, P))
     _, y = common.linear_intersect(line, vaplineH)
     return y
   
-  ideal_stages = common.curve_bouncer(liqlineH, vaplineH, xd, xb, eq_curve.inv, y_transform)
+  ideal_stages = common.curve_bouncer(vaplineH, liqlineH, xd, xb, eq_curve.inv, y_transform)
 
   return Rmin, R, min_stages, ideal_stages
 
