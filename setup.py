7adfe03a8ddda662fb002673ef9e9ff218505a3e
ethanmolnar06 <157933281+ethanmolnar06@users.noreply.github.com>
2024-04-11 19:52:35 -0400

    HOTFIX fix import statements for the package update version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.10'
+VERSION = '0.0.11'
 DESCRIPTION = 'A library of common chemical engineering calculations'
 LONG_DESCRIPTION = 'A personal project cataloging common chemical engineering calculations employing numerical methods'
 
