618b87c58ce841089bf136896e5cc5fda12af1b7
ethanmolnar06 <157933281+ethanmolnar06@users.noreply.github.com>
2024-05-07 02:26:56 -0400

    fix indent issue add output types add reynolds, reynolds_pipe, schmidt, sherwood, nusselt

--- chetoolbox/transport.py
+++ chetoolbox/transport.py
@@ -6,7 +6,7 @@ from . import common, props
 
 from scipy.optimize import root
 def PressureDrop(Q: float, mu: float, D: float, rho: float, epsilon: float = 0.10e-3) -> float: 
-  r"""
+  """
   Solve for the pressure drop of a straight pipe using the Darcy-Weisbach equation & Colebrook White equation.
   
   Parameters
@@ -17,7 +17,7 @@ def PressureDrop(Q: float, mu: float, D: float, rho: float, epsilon: float = 0.1
     Viscosity of the fluid in Pa*s (pascal seconds).
   D : float
     Pipe diameter in m (meters).
-  mu : float
+  rho : float
     Density of the fluid in kg/m^3 (kilograms per meter cubed).
   epsilon : float
     Absolute roughness value of the pipe in m (meters). Default value of 0.10e-3 meters is the roughness value of moderately-corroded carbon steel.
@@ -27,20 +27,19 @@ def PressureDrop(Q: float, mu: float, D: float, rho: float, epsilon: float = 0.1
   PressureDrop : float
     Pressure drop per unit length in Pa/m (pascals per meter).
   """
-  
   v = Q / (np.pi * ((D/2)**2))
   Re = rho * v * D / mu
   def ColebrookWhite(v):
-      f = v[0] # Unpack a vector of unknowns
-      return [ (1 / np.sqrt(f)) - 
-              (-2 * np.log10((epsilon/(3.7 * D)) + 2.51/(Re*np.sqrt(f))))] #subtract RHS from LHS. If each side is equivalent, we expect 0
+    f = v[0] # Unpack a vector of unknowns
+    return [ (1 / np.sqrt(f)) - 
+            (-2 * np.log10((epsilon/(3.7 * D)) + 2.51/(Re*np.sqrt(f))))] #subtract RHS from LHS. If each side is equivalent, we expect 0
   if Re<4000:
-      f = 64 / Re 
+    f = 64 / Re 
   else:
-      f = root(ColebrookWhite,[0.03]).x[0]
+    f = root(ColebrookWhite,[0.03]).x[0]
   return -(f * (rho / 2) * (v**2 /D))
 
-def free_mean_path(T_and_P: npt.NDArray, d_molecule: npt.NDArray | float):
+def free_mean_path(T_and_P: npt.NDArray, d_molecule: npt.NDArray | float) -> npt.NDArray | float:
   '''
   Calculates the mean free path of molecules in unrestricted diffusion.  
   
@@ -60,7 +59,7 @@ def free_mean_path(T_and_P: npt.NDArray, d_molecule: npt.NDArray | float):
   kB = 1.380649e-23 # J/K*mol
   return kB * T_and_P[:, 0] / (np.sqrt(2.) * T_and_P[:, 1] * np.pi * d_molecule**2)
 
-def diffuse_knudsen(MW: npt.NDArray, r_pore: float, T: float, porous: float | None = None, tort: float | None = None):
+def diffuse_knudsen(MW: npt.NDArray, r_pore: float, T: float, porous: float | None = None, tort: float | None = None) -> npt.NDArray | float:
   '''
   Calculates the knudsen diffusivity of molecules in through porous membranes.  
   
@@ -86,3 +85,120 @@ def diffuse_knudsen(MW: npt.NDArray, r_pore: float, T: float, porous: float | No
   if porous is not None and tort is not None:
     return D * porous / tort
   return .25 * D
+
+def reynolds(L: npt.NDArray | float, flowspeed: npt.NDArray | float, rho: float, mu: float) -> npt.NDArray | float:
+  '''
+  Calculates the Reynolds number of a dynamic fluid. If multiple characteristic lengths (L) and flow speeds (FS) are input, the resulting array will be of size FS x L. Units must cancel.
+  
+  Parameters
+  ----------
+  L : NDArray | float
+    Characteristic length of the system.
+  flowspeed : NDArray | float
+    Linear flow speed of the fluid, typically perpendicular to the characteristic length.
+  rho : float
+    Density of the fluid.
+  mu : float
+    Dynamic viscosity of the fluid.
+  
+  Returns
+  ----------
+  Re : NDArray | float
+    Reynolds number of the dynamic fluid (Unitless).
+  '''
+  L = np.atleast_1d(L)
+  flowspeed = np.atleast_1d(flowspeed).reshape(-1, 1)
+  Re = rho * L * flowspeed / mu
+  return Re[0] if Re.size == 1 else Re
+
+def reynolds_pipe(din: npt.NDArray | float, Q_dot: npt.NDArray | float, rho: float, mu: float) -> npt.NDArray | float:
+  '''
+  Calculates the Reynolds number of a dynamic fluid in a circular pipe. If multiple inner diameters (D) and flow rates (Q) are input, the resulting array will be of size Q x D. Units must cancel.
+  
+  Parameters
+  ----------
+  din : NDArray | float
+    Inner diameter of the pipe.
+  Q_dot : NDArray | float
+    Volumetric flowrate of the fluid through the pipe. If rho set == 1., mass flowrate (W_dot) may be used.
+  rho : float
+    Density of the fluid.
+  mu : float
+    Dynamic viscosity of the fluid.
+  
+  Returns
+  ----------
+  Re : NDArray | float
+    Reynolds number of the dynamic fluid (Unitless).
+  '''
+  din = np.atleast_1d(din)
+  Q_dot = np.atleast_1d(Q_dot).reshape(-1, 1)
+  A = np.pi * din**2 / 4.
+  Re = rho * Q_dot * din / (mu * A)
+  return Re[0] if Re.size == 1 else Re
+
+def schmidt(massDiff: npt.NDArray | float, rho: float, mu: float) -> npt.NDArray | float:
+  '''
+  Calculates the Schmidt number of a dynamic fluid. Units must cancel.
+  
+  Parameters
+  ----------
+  massDiff : NDArray | float
+    Rate of mass diffusion through the fluid.
+  rho : float
+    Density of the fluid.
+  mu : float
+    Dynamic viscosity of the fluid.
+  
+  Returns
+  ----------
+  Sc : NDArray | float
+    Schmidt number of the dynamic fluid (Unitless).
+  '''
+  return mu / (rho * massDiff)
+
+def sherwood(massDiff: npt.NDArray | float, L: npt.NDArray | float, h: float) -> npt.NDArray | float:
+  '''
+  Calculates the Sherwood number of a dynamic fluid. If multiple characteristic lengths (L) and mass diffusions (MD) are input, the resulting array will be of size MD x L. Units must cancel.
+  
+  Parameters
+  ----------
+  massDiff : NDArray | float
+    Rate of mass diffusion through the fluid.
+  L : NDArray | float
+    Characteristic length of the system.
+  h : float
+    Convective mass transfer film coefficient.
+  
+  Returns
+  ----------
+  Sh : NDArray | float
+    Sherwood number of the dynamic fluid (Unitless).
+  '''
+  L = np.atleast_1d(L)
+  massDiff = np.atleast_1d(massDiff).reshape(-1, 1)
+  Sh = h * L / massDiff
+  return Sh[0] if Sh.size == 1 else Sh
+
+def nusselt(thermCond: npt.NDArray | float, L: npt.NDArray | float, h: float) -> npt.NDArray | float:
+  '''
+  Calculates the Nusselt number for a dynamic fluid. If multiple characteristic lengths (L) and thermal conductivity (TC) are input, the resulting array will be of size TC x L. Units must cancel.
+  
+  Parameters
+  ----------
+  thermCond : NDArray | float
+    Thermal conductivity of the fluid.
+  L : NDArray | float
+    Characteristic length of the system.
+  h : float
+    Convective heat transfer coefficient.
+  
+  Returns
+  ----------
+  Nu : NDArray | float
+    Nusselt number of the dynamic fluid (Unitless).
+  '''
+  L = np.atleast_1d(L)
+  thermCond = np.atleast_1d(thermCond).reshape(-1, 1)
+  Nu = h * L / thermCond
+  return Nu[0] if Nu.size == 1 else Nu
